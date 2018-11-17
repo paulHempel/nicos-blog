83f3bdc98f52366c986d5029f03a9f8616501707
spaggo <nico.imhof@t-online.de>
2018-11-17 16:26:36 +0100

    changes

---
+++ Post hochladen.bat
@@ -0,0 +1,14 @@
+ECHO OFF
+ECHO Hallo Nico, du nimmst jetzt Aenderungen an deiner Website vor!
+PAUSE
+ECHO Ueberpruefe ob externe Aenderungen vorliegen…
+git pull
+ECHO Fuege alle Aenderungen hinzu…
+git add -A
+ECHO Fuege Aenderungen zusammen
+git commit 
+Nicos Git script adding changes
+ECHO Sende Aenderungen an externen Server…
+git push
+ECHO Aenderungen werden jetzt automatisch verarbeitet und auf Website veroeffentlicht
+PAUSE
